7280349875c2c04654ab19b53138feb60b61e3ba
nebula-aadil <aadil.shaikh@nebula-technology.com>
2025-09-13 11:11:06 +0530

    add a new feature filter rooms

--- controllers/filterController.js
+++ controllers/filterController.js
@@ -4,25 +4,97 @@ import { Property } from "../models/Property.js";
 
 
 
-export const PricesBaseFilter = catchAsyncError(async(req , res , next) =>{
-    const {sort} = req.query;
+export const PricesBaseFilter = catchAsyncError(async (req, res, next) => {
+    const { sort } = req.query;
 
     let sortOption = {};
 
-    if(sort === "lowToHigh"){
-        sortOption = {price: 1};
-    }else if(sort === "highToLow"){
-        sortOption = {price: -1};
+    if (sort === "lowToHigh") {
+        sortOption = { price: 1 };
+    } else if (sort === "highToLow") {
+        sortOption = { price: -1 };
+    } else if (sort === "ratingHighToLow") {
+        sortOption = { avgRating: -1 };
     }
-    
-    const properties = await Property.find({expired : false}).sort(sortOption);
-   if(!properties || properties.length === 0){
-    return next(new ErrorHandler("No properties found", 404));
-   }
-   
-   res.status(200).json({
-    success: true,
-    count : properties.length,
-    properties,
-   })
+
+    const properties = await Property.find({ expired: false }).sort(sortOption);
+    if (!properties || properties.length === 0) {
+        return next(new ErrorHandler("No properties found", 404));
+    }
+
+    res.status(200).json({
+        success: true,
+        count: properties.length,
+        properties,
+    })
+})
+
+
+export const FilterRooms = catchAsyncError(async (req, res, next) => {
+    const { priceRange, capacity, view, features, bedType, sort } = req.body;
+
+    let query = { expired: false };
+
+    //Price Filter
+    if (priceRange) {
+        if (priceRange === "0-1000") {
+            query.price = { $gte: 0, $lte: 1000 };
+        } else if (priceRange === "1000-2000") {
+            query.price = { $gte: 1000, $lte: 2000 };
+        } else if (priceRange === "2000-4000") {
+            query.price = { $gte: 2000, $lte: 4000 };
+        } else if (priceRange === "4000+") {
+            query.price = { $gte: 4000 };
+        }
+    }
+
+    // Capacity Filter
+    if (capacity) {
+        if (capacity === "1-2") {
+            query.maxGuests = { $gte: 1, $lte: 2 };
+        } else if (capacity === "2-4") {
+            query.maxGuests = { $gte: 2, $lte: 4 };
+        } else if (capacity === "5+") {
+            query.maxGuests = { $gte: 5 };
+        }
+    }
+
+    //  View filter
+    if (view && view !== "All") {
+        query.views = view;
+    }
+
+    //  Features filter (array includes)
+    if (features && features.length > 0) {
+        query.facilities = { $all: features };
+    }
+    // Bed type filter
+    if (bedType) {
+        query.bedType = bedType;
+    }
+
+    // Sorting
+    let sortOption = {};
+    if (sort === "lowToHigh") {
+        sortOption = { price: 1 };
+    } else if (sort === "highToLow") {
+        sortOption = { price: -1 };
+    } else if (sort === "ratingHighToLow") {
+        sortOption = { avgRating: -1 };
+    } else {
+        sortOption = { propertyPostedOn: -1 };
+    }
+
+    //  Final Query
+    const properties = await Property.find(query).sort(sortOption);
+
+    if (!properties || properties.length === 0) {
+        return next(new ErrorHandler("No properties found", 404));
+    }
+
+    res.status(200).json({
+        success: true,
+        count: properties.length,
+        properties,
+    });
 })
--- routes/filterRoutes.js
+++ routes/filterRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { isAuthorized } from "../middlewares/authMiddleware.js";
-import { PricesBaseFilter } from "../controllers/filterController.js";
+import { FilterRooms, PricesBaseFilter } from "../controllers/filterController.js";
 
 const router = express.Router();
 
 router.get("/priceFilter", isAuthorized, PricesBaseFilter);
+router.post("/roomFilter", isAuthorized, FilterRooms);
 
 export default router;
